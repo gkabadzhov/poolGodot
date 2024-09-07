0f497938012243f311e9424e001b4565fda01539
Georgi Kabadzhov <kabadzhov.georgi@gmail.com>
2024-09-07 12:30:59 +0300

    adding different behaviour on collision for white balls (intended for future character types)

--- Scripts/white_cue.gd
+++ Scripts/white_cue.gd
@@ -1,7 +1,21 @@
 extends RigidBody2D
 
+@export var should_bounce: bool = false 
+
 var direction = Vector2.ZERO
 var force_applied = false
+
+func _ready():
+	if physics_material_override:
+		physics_material_override.bounce = 0.5
+		linear_damp = 0.5
+		angular_damp = 0.3
+
+func _integrate_forces(state):
+	if state.get_contact_count() > 0:
+		if not should_bounce:
+			linear_velocity = Vector2.ZERO
+			angular_velocity = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
